e2e2bba8747db52b8f9d7738f6fdccb9f5922f10
vanderc <cnvandermark@byui.edu>
2023-11-08 10:59:43 -0800

    commit

---
+++ week02/dom-basics.js
@@ -0,0 +1,22 @@
+const newParagraph = document.createElement("p");
+newParagraph.innerText = "Added with Javascript!";
+document.body.appendChild(newParagraph);
+
+const newImage = document.createElement("img");
+newImage.setAttribute("src", "https://picsum.photos/200");
+newImage.setAttribute("alt", "Description of image");
+document.body.appendChild(newImage);
+
+const newDiv = document.createElement("div");
+newDiv.innerHTML = "<ul><li>One</li><li>Two</li><li>Three</li></ul>";
+document.body.appendChild(newDiv);
+
+const newSection = document.createElement("section");
+const newH2 = document.createElement("h2");
+newH2.innerText = "CSE 121b";
+newSection.appendChild(newH2);
+const newP = document.createElement("p");
+newH2.innerText = "Welcome to Javascript Language";
+newSection.appendChild(newP);
+
+document.body.appendChild(newSection);
