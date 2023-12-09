0f14178cbd4a09d02e29531bf8ed7c0da172da15
vanderc <cnvandermark@byui.edu>
2023-12-09 13:13:15 -0800

    commit

---
+++ scripts/project.js
@@ -0,0 +1,45 @@
+let weather = document.getElementById("weatherDiv");
+let searchButton = document.getElementById("search");
+let cityReference = document.getElementById("city");
+let key = "b4f039a7f4409449f111d776279a66e0";
+
+
+let getWeather = (city) => {
+  let lat = city[0].lat;
+  let lon = city[0].lon;
+  let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=imperial&appid=${key}`;
+  cityReference.value = "";
+  fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => {
+      console.log(data);
+      weather.innerHTML = 
+      `<h1>Weather for ${city[0].name}</h1>
+      <h2>Forecast for Today</h2>
+      <h3>${data.current.weather[0].description}</h3>
+      <img src="https://openweathermap.org/img/w/${data.current.weather[0].icon}.png">
+      <h2>Current Temperature</h2>
+      <h3>${data.current.temp}&#176;F</h3>
+      <h2>Today's Min</h2>
+      <h3>${data.daily[0].temp.min}&#176;F</h3>
+      <h2>Today's Max</h2>
+      <h3>${data.daily[0].temp.max}&#176;F</h3>`;
+    }) 
+    .catch(() => {
+      weather.innerHTML = `<h3 class="msg">Please try again</h3>`;
+    });
+};
+
+async function getCityData() {
+  let cityValue = cityReference.value;
+  if (cityValue.length == 0) {
+    weather.innerHTML = `<h3 class="msg">Please enter a city name</h3>`;
+  }
+  else {
+    let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${key}`);
+    let cityOutput = await response.json();
+    getWeather(cityOutput);
+  }
+};
+
+search.addEventListener("click", getCityData);
